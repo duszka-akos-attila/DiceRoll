802ed8bb0a0172a37eaa866cfa44e7c14023fa78
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-24 20:49:54 +0200

    Collected similar methods into one method.

--- src/main/java/game/DiceManager.java
+++ src/main/java/game/DiceManager.java
@@ -2,6 +2,70 @@ package game;
 
 public class DiceManager {
 
+    public void rollDice(Game game, String direction){
+        Dice dice = game.getDice();
+        int[] position = new int[2];
+        position[0]=dice.getPosition()[0];
+        position[1]=dice.getPosition()[1];
+        int onTop=dice.getOnTop();
+        int onFront=dice.getOnFront();
+
+        switch (direction){
+            case("left"):
+                if(canRollLeft(game)) {
+                    position[1] -= 1;
+                    onTop=pairOf(getLeft(dice));
+                    onFront=dice.getOnFront();
+                }
+                else{
+                    return;
+                }
+                break;
+
+            case("right"):
+                if(canRollRight(game)) {
+                    position[1] += 1;
+                    onTop=getLeft(dice);
+                    onFront=dice.getOnFront();
+                }
+                else{
+                    return;
+                }
+                break;
+
+            case("up"):
+                if(canRollUp(game)) {
+                    position[0] -= 1;
+                    onTop=dice.getOnFront();
+                    onFront=pairOf(dice.getOnTop());
+                }
+                else{
+                    return;
+                }
+                break;
+
+            case("down"):
+                if(canRollDown(game)) {
+                    position[0] += 1;
+                    onTop=pairOf(dice.getOnFront());
+                    onFront=dice.getOnTop();
+                }
+                else{
+                    return;
+                }
+                break;
+
+            default:
+
+        }
+
+        dice.setPosition(position);
+        dice.setOnTop(onTop);
+        dice.setOnFront(onFront);
+
+        game.setDice(dice);
+    }
+
     public void rollDown(Game game){
         Dice dice = game.getDice();
         if(canRollDown(game)){
@@ -77,6 +141,49 @@ public class DiceManager {
 
             game.setDice(dice);
         }
+    }
+
+
+    public boolean canRoll(Game game, String direction){
+        Dice dice = game.getDice();
+        Field field = game.getField();
+        int[] targetPosition = {dice.getPosition()[0], dice.getPosition()[1]};
+        int numberOnField;
+
+        switch (direction) {
+            case ("left"):
+                if(game.getDice().getPosition()[1]-1>-1){
+                    targetPosition[1] -= 1;
+                    numberOnField = field.getFields()[targetPosition[0]][targetPosition[1]];
+                    return dice.getOnTop() == numberOnField;
+                }
+                break;
+
+            case ("right"):
+                if(game.getDice().getPosition()[1]+1<5){
+                    targetPosition[1] += 1;
+                    numberOnField = field.getFields()[targetPosition[0]][targetPosition[1]];
+                    return dice.getOnTop() == numberOnField;
+                }
+                break;
+
+            case ("up"):
+                if(dice.getPosition()[0]-1>-1){
+                    targetPosition[0] -= 1;
+                    numberOnField = field.getFields()[targetPosition[0]][targetPosition[1]];
+                    return dice.getOnTop() == numberOnField;
+                }
+                break;
+
+            case ("down"):
+                if(dice.getPosition()[0]+1<6){
+                    targetPosition[0] += 1;
+                    numberOnField = field.getFields()[targetPosition[0]][targetPosition[1]];
+                    return dice.getOnTop() == numberOnField;
+                }
+                break;
+        }
+        return false;
     }
 
 
