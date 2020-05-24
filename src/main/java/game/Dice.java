3f6fd6f8f11b3e6f42b07033d846a8d1b8251636
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-24 14:47:15 +0200

    Created Field and Dice object classes for the game.

--- src/main/java/game/Dice.java
+++ src/main/java/game/Dice.java
@@ -1,4 +1,14 @@
 package game;
 
+@lombok.Data
 public class Dice {
+    private int onTop;
+    private int onFront;
+    private int[] position;
+
+    public Dice() {
+        this.onTop = 6;
+        this.onFront = 4;
+        this.position = new int[] {0,0};
+    }
 }
--- src/main/java/game/Field.java
+++ src/main/java/game/Field.java
@@ -1,4 +1,17 @@
 package game;
 
 public class Field {
+    private final int[][] fields;
+    public Field(){
+        fields=new int[6][5];
+        for(int i=0;i<6;i++){
+            for(int j=0;j<5;j++) {
+                fields[i][j]=(int)(Math.random()*6)+1;
+            }
+        }
+    }
+
+    public int[][] getFields() {
+        return fields;
+    }
 }
