93ac8a6dc877dba9132771c76942e1ed21f2270c
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-25 20:02:06 +0200

    Created empty test class for DiceManager.

---
+++ src/test/java/game/DiceManagerTest.java
@@ -0,0 +1,24 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceManagerTest {
+
+    @Test
+    void rollDice() {
+    }
+
+    @Test
+    void canRoll() {
+    }
+
+    @Test
+    void getBiggerFromPair() {
+    }
+
+    @Test
+    void getLeft() {
+    }
+}
