07955f462f076eb571d31db8e1ad4bbe2182e85b
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-25 20:01:00 +0200

    Splitted constructor method to constructor and randomizeFields methods.

--- src/main/java/game/Field.java
+++ src/main/java/game/Field.java
@@ -2,11 +2,20 @@ package game;
 
 public class Field {
     private final int[][] fields;
+
     public Field(){
         fields=new int[6][5];
         for(int i=0;i<6;i++){
             for(int j=0;j<5;j++) {
-                fields[i][j]=(int)(Math.random()*6)+1;
+                fields[i][j]=0;
+            }
+        }
+    }
+
+    public void randomizedFields(){
+        for(int i=0;i<6;i++){
+            for(int j=0;j<5;j++) {
+                this.fields[i][j]=(int)(Math.random()*6)+1;
             }
         }
     }
