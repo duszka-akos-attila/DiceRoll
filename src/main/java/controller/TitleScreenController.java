d8beeca32eba85fd2a3a0b4df68b7508696ca51d
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-26 22:36:38 +0200

    Added standardGamePressed method for starting standard game scene.

--- src/main/java/controller/TitleScreenController.java
+++ src/main/java/controller/TitleScreenController.java
@@ -1,10 +1,29 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class TitleScreenController {
 
     public void exitPressed(ActionEvent actionEvent){
         System.exit(0);
     }
+
+    public void standardGamePressed(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/standardGamePlayScene.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene=(Scene) ((Node) actionEvent.getSource()).getScene();
+        scene.getStylesheets().add(getClass().getResource("CubeIsOnTop.css").toExternalForm());
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+
 }
