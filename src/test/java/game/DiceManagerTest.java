47052f803c33cf68d8dac18338b40f55ab4229ee
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-26 13:58:35 +0200

    Started the testing.

--- src/test/java/game/DiceManagerTest.java
+++ src/test/java/game/DiceManagerTest.java
@@ -4,10 +4,24 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DiceManagerTest {
+public class DiceManagerTest {
 
     @Test
     void rollDice() {
+        Game testGame = new Game();
+        Dice controllDice = testGame.getDice();
+        Dice testDice = testGame.getDice();
+        DiceManager dm = new DiceManager();
+        dm.rollDice(testGame, "left");
+        testDice = testGame.getDice();
+        assertEquals(controllDice,testDice);
+
+
+
+//        dm.rollDice(testGame, "right");
+//        testDice = testGame.getDice();
+//        controllDice.setPosition(new int[]{0,1});
+//        assertEquals(controllDice,testDice);
     }
 
     @Test
