57dbca26ca8ce8e4e8cc48d82533b483c5b65f06
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-26 14:09:49 +0200

    Made fields variable not final and added new method loadStandardFields, which load the fields with the newly defined standardFields array.

--- src/main/java/game/Field.java
+++ src/main/java/game/Field.java
@@ -1,7 +1,15 @@
 package game;
 
 public class Field {
-    private final int[][] fields;
+    private int[][] fields;
+    private final int[][] standardFields = {
+            {4,6,2,5,1},
+            {5,2,1,0,5},
+            {6,2,3,1,5},
+            {6,4,6,3,6},
+            {5,3,4,5,1},
+            {3,6,0,3,3}
+    };
 
     public Field(){
         fields=new int[6][5];
@@ -18,6 +26,10 @@ public class Field {
                 this.fields[i][j]=(int)(Math.random()*6)+1;
             }
         }
+    }
+
+    public void loadStandardFields(){
+        this.fields=standardFields;
     }
 
     public int[][] getFields() {
