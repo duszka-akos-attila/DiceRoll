b18219d425e7bf8f334407ec8385a50108213ef5
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 08:48:44 +0200

    Documented the remaining methods of the DiceManager class.

--- src/main/java/game/DiceManager.java
+++ src/main/java/game/DiceManager.java
@@ -1,6 +1,20 @@
 package game;
 
+/**
+ * This class is responsible for the management of the Dice.
+ */
+
 public class DiceManager {
+
+    /**
+     * This method checks the game if the dice can roll to the
+     * given direction and if it's can then calculate the state
+     * of the dice after rolling using: {@see canRoll}, {@see pairOf}, {@see getLeft}
+     * @param game A game type parameter is needed, because the canRoll method
+     *             have to check both the Field object and the Dice object
+     *             to decide whether the dice can roll or not.
+     * @param direction The Direction where the player want to move the dice.
+     */
 
     public void rollDice(Game game, String direction){
         Dice dice = game.getDice();
@@ -66,6 +80,13 @@ public class DiceManager {
         game.setDice(dice);
     }
 
+    /**
+     * The canRoll method checks for the {@see rollDice} method if a dice can roll to a given direction.
+     * @param game It's need both a field and a dice to calculate the move.
+     * @param direction The direction where the player want to move.
+     * @return with true if the player can move to the given direction and false if not.
+     */
+
     public boolean canRoll(Game game, String direction){
         Dice dice = game.getDice();
         Field field = game.getField();
@@ -116,9 +137,21 @@ public class DiceManager {
         return false;
     }
 
+    /**
+     * This method calculates the pair of the dice's given side.
+     * @param n is the number on the dice which pair we want to find.
+     * @return the pair of the {@param n}.
+     */
+
     public int pairOf(int n){
         return 7-n;
     }
+
+    /**
+     * By the dice's two known face, this method can calculate the bigger one of the unknown face pair.
+     * @param dice requires the dice's two face to calculate the other faces.
+     * @return the bigger number from the unknown face pair.
+     */
 
     public int maxRemaining(Dice dice){
         int biggerFromTopPair = getBiggerFromPair(dice.getOnTop());
@@ -126,12 +159,24 @@ public class DiceManager {
         return 15-biggerFromFrontPair-biggerFromTopPair;
     }
 
+    /**
+     * Calculate the bigger number of a pair.
+     * @param n a face which will be compared to it's pair.
+     * @return the number of the face which has the bigger value.
+     */
+
     public int getBiggerFromPair(int n){
         if(n<4){
             return pairOf(n);
         }
         return n;
     }
+
+    /**
+     * Calculating the left face of dice with the know two faces. {@link Dice}
+     * @param dice needs the dice object's two known face to determinate the left face.
+     * @return the left face of the dice.
+     */
 
     public int getLeft(Dice dice){
         int onTop = dice.getOnTop();
