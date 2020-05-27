8e075d52e0295af2f79b604c518c3ef67456161c
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 07:38:59 +0200

    Updated the documentation in package-info file.

--- src/main/java/main/package-info.java
+++ src/main/java/main/package-info.java
@@ -3,6 +3,8 @@
  * Contains the Main class which starts the other class in the package,
  * the DiceRollApplication. The DiceRollApplication class is responsible
  * for the start of the game's Graphical User Interface.
+ * {@link main.Main}
+ * {@link main.DiceRollApplication}
  */
 
 package main;
