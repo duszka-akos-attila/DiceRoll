f78b15af590379d90bea188ba1ab693fe6809f22
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 07:38:41 +0200

    Updated the documentation in Main class.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,7 +2,17 @@ package main;
 
 import javafx.application.Application;
 
+/**
+ * The Main class which starts first when running the game's packaged jar file.
+ */
+
 public class Main {
+
+    /**
+     * The main method which starts the DiceRollApplication.
+     * @param args are the given parameters what the user set when starting the program.
+     */
+
     public static void main(String[] args) {
         Application.launch(DiceRollApplication.class, args);
     }
