c7ceee2ab904deed308fe2f1180d47e1f62500ef
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 08:53:22 +0200

    Added documentation to the Dice object.

--- src/main/java/game/Dice.java
+++ src/main/java/game/Dice.java
@@ -1,10 +1,36 @@
 package game;
 
+/**
+ * This class representing the Dice in the game.
+ */
+
 @lombok.Data
 public class Dice {
+
+    /**
+     * This variable contains the number on the top face of the dice.
+     */
+
     private int onTop;
+
+    /**
+     * This variable contains the number on the front face of the dice.
+     */
+
     private int onFront;
+
+    /**
+     * This array contains the coordinates of the dice on the play field.
+     * {@link Field}
+     */
+
     private int[] position;
+
+    /**
+     * The standard constructor of the Dice is setting onTop variable to six,
+     * onFront variable to 4 and the position of the dice to the top left corner
+     * of the Field.
+     */
 
     public Dice() {
         this.onTop = 6;
