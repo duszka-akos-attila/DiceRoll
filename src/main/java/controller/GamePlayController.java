2db05b9c3bc25e8f2fb83a933e41602d2f5ce528
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 02:21:21 +0200

    Created updateScreen method and replaced settings in initialize method with updateScreen.

--- src/main/java/controller/GamePlayController.java
+++ src/main/java/controller/GamePlayController.java
@@ -47,16 +47,8 @@ public class GamePlayController {
                 game.randomizedGame();
                 break;
         }
-        onTopImageView.setImage(new Image(getClass().getResource("/pictures/"+ game.getDice().getOnTop()+".png").toExternalForm()));
-        onFrontImageView.setImage(new Image(getClass().getResource("/pictures/"+ game.getDice().getOnFront()+".png").toExternalForm()));
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 5; j++){
-                ImageView view = (ImageView) playField.getChildren().get(i * 5 + j);
-                view.setImage(new Image(getClass().getResource("/pictures/"+ game.getField().getFields()[i][j]+".png").toExternalForm()));
-            }
-        }
-
-        ImageView view = (ImageView) playField.getChildren().get(0);
+        
+        updateScreen();
     }
 
     public void forfeit(ActionEvent actionEvent) throws IOException {
@@ -67,6 +59,21 @@ public class GamePlayController {
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
+        }
+    }
+
+    public void updateScreen(){
+        onTopImageView.setImage(new Image(getClass().getResource("/pictures/"+ game.getDice().getOnTop()+".png").toExternalForm()));
+        onFrontImageView.setImage(new Image(getClass().getResource("/pictures/"+ game.getDice().getOnFront()+".png").toExternalForm()));
+        int dicePosition = game.getDice().getPosition()[0] * 5 + game.getDice().getPosition()[1];
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 5; j++){
+                ImageView view = (ImageView) playField.getChildren().get(i * 5 + j);
+                if(dicePosition == i * 5 + j){
+                    view.setImage(new Image(getClass().getResource("/pictures/b"+ game.getField().getFields()[i][j]+".png").toExternalForm()));
+                }
+                view.setImage(new Image(getClass().getResource("/pictures/"+ game.getField().getFields()[i][j]+".png").toExternalForm()));
+            }
         }
     }
 }
