fb24f1ad2b6f1f17a79bcc411a59ac5340f875b1
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 05:43:13 +0200

    Created gameWonScreenController, the name submission methods and copied the controlling buttons event manager from gameSurrendedScreenController.

---
+++ src/main/java/controller/gameWonScreenController.java
@@ -0,0 +1,53 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class gameWonScreenController {
+
+    @FXML
+    private TextField playerNameTextField;
+
+    @FXML
+    private GridPane playerNameForm;
+
+    @FXML
+    private GridPane controlGrid;
+
+    public void submitResultByEnter(KeyEvent keyEvent) throws IOException{
+        if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+            updateScreen();
+        }
+    }
+    public void submitResult(ActionEvent actionEvent) throws IOException{
+        updateScreen();
+    }
+
+    public void updateScreen(){
+        playerNameForm.setVisible(false);
+        controlGrid.setVisible(true);
+    }
+
+    public void exitPressed(ActionEvent actionEvent){
+        System.exit(0);
+    }
+
+    public void mainMenuPressed(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/titleScreen.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
