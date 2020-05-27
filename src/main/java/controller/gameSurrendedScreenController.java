a548746ce640a30dfd40ff19e4a59d529fd62f17
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 12:51:36 +0200

    Added logging for the gameSurrendedScreenController.

--- src/main/java/controller/gameSurrendedScreenController.java
+++ src/main/java/controller/gameSurrendedScreenController.java
@@ -6,17 +6,29 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import result.ResultManager;
 
 import java.io.IOException;
 
 public class gameSurrendedScreenController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ResultManager.class);
+
     public void exitPressed(ActionEvent actionEvent){
-        ResultManager.closeEMF();
+        try {
+            ResultManager.closeEMF();
+        }
+        catch (Exception e){
+            logger.error(e.toString());
+        }
         System.exit(0);
     }
 
     public void mainMenuPressed(ActionEvent actionEvent) throws IOException {
+        logger.trace("The player pressed Main menu button.");
+        logger.trace("Loading titleScreen scene.");
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/titleScreen.fxml"));
         Parent root = fxmlLoader.load();
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
