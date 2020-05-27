3f5051db34113dc756b5ae6e8069520ecc8f5c3b
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 04:42:29 +0200

    Created the Result object with documentation.

---
+++ src/main/java/result/Result.java
@@ -0,0 +1,34 @@
+package result;
+
+/**
+ * This class representing those games which was won by the player {@code name},
+ * and the number of moves {@code moves} which was needed to win the game.
+ */
+
+public class Result {
+
+    /**
+     * An automatically generated identifier by the database.
+     */
+
+    private long id;
+
+    /**
+     * The name what the player set to be represented with in the leaderboard.
+     */
+
+    private String name;
+
+    /**
+     * Number of moves what take the player to won the game.
+     */
+
+    private int moves;
+
+    /**
+     * The game mode what the player won in.
+     */
+
+    private String gameMode;
+
+}
