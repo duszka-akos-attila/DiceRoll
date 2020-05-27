30293102d54a7714c48d0fbd18dc47dc43436393
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 05:48:42 +0200

    Added icon to the application.

--- src/main/java/main/DiceRollApplication.java
+++ src/main/java/main/DiceRollApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class DiceRollApplication extends Application {
@@ -14,6 +15,7 @@ public class DiceRollApplication extends Application {
         stage.setTitle("Dice Roll");
         stage.setResizable(false);
         stage.setScene(new Scene(root));
+        stage.getIcons().add(new Image(getClass().getResource("/pictures/b6.png").toExternalForm()));
         stage.show();
     }
 }
