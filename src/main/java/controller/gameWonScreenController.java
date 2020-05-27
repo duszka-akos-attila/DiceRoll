b86f1ff4552096c0ecc32ab12660950315bb4e38
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 10:39:48 +0200

    Added database upload call in gameWonScreenController.

--- src/main/java/controller/gameWonScreenController.java
+++ src/main/java/controller/gameWonScreenController.java
@@ -12,7 +12,10 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import main.DiceRollApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import result.Result;
+import result.ResultManager;
 
 import java.io.IOException;
 
@@ -29,15 +32,34 @@ public class gameWonScreenController {
 
     private final Result result = DiceRollApplication.result;
 
+    private static final Logger logger = LoggerFactory.getLogger(gameWonScreenController.class);
+
     public void submitResultByEnter(KeyEvent keyEvent) throws IOException{
         if (keyEvent.getCode().equals(KeyCode.ENTER) && playerNameTextField.getText().length() > 2) {
+            logger.trace("ENTER pressed.");
             result.setName(playerNameTextField.getText());
+            logger.info("The player's name is: {}",result.getName());
+            try{
+                ResultManager.uploadResult();
+            }
+            catch (Exception e){
+                logger.error(e.toString());
+            }
+            ResultManager.closeEMF();
             updateScreen();
         }
     }
     public void submitResult(ActionEvent actionEvent) throws IOException{
         if(playerNameTextField.getText().length() > 2) {
             result.setName(playerNameTextField.getText());
+            logger.info("The player's name is: {}",result.getName());
+            try{
+                ResultManager.uploadResult();
+            }
+            catch (Exception e){
+                logger.error(e.toString());
+            }
+            ResultManager.closeEMF();
             updateScreen();
         }
     }
@@ -48,10 +70,12 @@ public class gameWonScreenController {
     }
 
     public void exitPressed(ActionEvent actionEvent){
+        logger.trace("Program finished by user!");
         System.exit(0);
     }
 
     public void mainMenuPressed(ActionEvent actionEvent) throws IOException {
+        logger.trace("Returning to the title screen.");
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/titleScreen.fxml"));
         Parent root = fxmlLoader.load();
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
