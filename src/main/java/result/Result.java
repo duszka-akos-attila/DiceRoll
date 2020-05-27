00e9e56b4828547a2ba5193bad7730d1173c60c4
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 10:02:41 +0200

    Added database structure defining annotations.

--- src/main/java/result/Result.java
+++ src/main/java/result/Result.java
@@ -1,35 +1,42 @@
 package result;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * This class representing those games which was won by the player {@code name},
  * and the number of moves {@code moves} which was needed to win the game.
  * @see game
  */
+@Entity
 @lombok.Data
 public class Result {
 
     /**
      * An automatically generated identifier by the database.
      */
-
+    @Id
+    @GeneratedValue
     private long id;
 
     /**
      * The name what the player set to be represented with in the leaderboard.
      */
-
+    @Column (nullable = false)
     private String name;
 
     /**
      * Number of moves what take the player to won the game.
      */
-
+    @Column (nullable = false)
     private int moves;
 
     /**
      * The game mode what the player won in.
      */
-
+    @Column (nullable = false)
     private String gameMode;
 
     /**
