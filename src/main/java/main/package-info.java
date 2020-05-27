70f896c0d89206613937b82347e463d0b20490d4
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-05-27 07:25:42 +0200

    Added package-info file to the main package.

---
+++ src/main/java/main/package-info.java
@@ -0,0 +1,8 @@
+
+/**
+ * Contains the Main class which starts the other class in the package,
+ * the DiceRollApplication. The DiceRollApplication class is responsible
+ * for the start of the game's Graphical User Interface.
+ */
+
+package main;
